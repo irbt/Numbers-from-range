330663b14178b44f5ca0e1e257a13bff0d02ecbb
Emmanuel Marchal <81331382+irbt@users.noreply.github.com>
2022-12-02 14:09:57 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Given an array of integers, write a query that returns list of numbers greater than 30 and less than 100.
+
+namespace Programm{
+    class NumbersFromRangeTask{
+
+        public static void Main (){
+            //création de la liste
+            List <int> Numbers = new  List <int>{ 30, 54, 3, 14, 25, 82, 1, 100, 23, 95 };
+
+            
+
+        }
+    }
+}
